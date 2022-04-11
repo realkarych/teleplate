7a0ea9a68d964d2670a14763b46e4bab82f2ec20
devkarych <kaarych@gmail.com>
2022-04-11 16:56:24 +0300

    Updated config file.

--- bot/config.py
+++ bot/config.py
@@ -5,11 +5,13 @@ import os
 
 @dataclass
 class Bot:
+    """DAO: Bot config"""
     token: str
 
 
 @dataclass
 class DB:
+    """DAO: Database config"""
     host: str
     db_name: str
     user: str
@@ -18,20 +20,28 @@ class DB:
 
 @dataclass
 class Config:
+    """DAO: Configuration"""
     bot: Bot
     db: DB
 
 
-def load():
-    """Load bot configuration"""
+def load() -> Config:
+    """Load and returns bot configuration data"""
+
     config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/bot.ini"
+
+    # If developer wasn't created bot.ini configuration file, raising an exception
+    if not os.path.exists(config_file_path):
+        raise NotImplementedError("bot.ini wasn't created!")
+
     config = configparser.ConfigParser()
     config.read(config_file_path)
 
-    # Parse .ini config
+    # Get .ini config "blocks"
     tg_bot = config["tg_bot"]
     db = config["db"]
 
+    # Get & return config data instance
     return Config(
         bot=Bot(
             token=tg_bot["token"]
